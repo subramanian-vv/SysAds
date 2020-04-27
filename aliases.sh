e197c756061792343674551dc119a6c0f3d3c065
root <koyilnet@gmail.com>
2020-04-27 22:14:47 +0530

    initial commit

---
+++ aliases.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+chmod +x /root/userGenerate.sh
+alias userGenerate='~/root/userGenerate.sh'
+chmod +x /root/permit.sh
+alias permit='~/root/permit.sh'
+chmod +x /root/autoSchedule.sh
+alias autoSchedule='~/root/autoSchedule.sh'
+chmod +x /root/attendance.sh
+alias attendance='~/root/attendance.sh'
+chmod +x /root/record.sh
+alias record='~/record.sh'
+chmod +x /root/finalAttendance.sh
+alias finalAttendance='~/root/finalAttendance.sh'
